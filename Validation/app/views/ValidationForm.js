999969954f0ba43acf37830878dd6f19f15190b3
tushar-kulkarni31 <santushar31@gmail.com>
2017-01-07 23:19:45 +0530

    Validation
    New changes merged

--- Validation/app/views/ValidationForm.js
+++ Validation/app/views/ValidationForm.js
@@ -6,7 +6,10 @@ define([
     'dojo/_base/declare',
     'dijit/form/Form',
     'dijit/_WidgetsInTemplateMixin',
-    'dojo/text!./ValidationForm.html'
+    'dojo/text!./ValidationForm.html',
+    'dojox/layout/TableContainer',
+    'dijit/form/ValidationTextBox',
+    'dojox/validate/web'
 ],function(declare,Form,_WidgetsInTemplateMixin,template){
     return declare([Form,_WidgetsInTemplateMixin],{
         templateString: template
